ea54be3ce5218d3cf032ec9f657ede6cd146c6c7
Ícaro Xavier <74971260+xaviericaro@users.noreply.github.com>
2025-06-16 20:42:52 -0300

    Update Conversor_Celsius_Fahrenheit.py

--- Conversor_Celsius_Fahrenheit.py
+++ Conversor_Celsius_Fahrenheit.py
@@ -1,7 +1,1 @@
-("--------------------------------------------------")
-("      Conversor de Celsius para Fahrenheit        ")
-("--------------------------------------------------")
-Celsius = float(input("Digite a temperatura em Celsius: "))
-Fahrenheit = (Celsius * 1.8) + 32
-print("A temperatura em Fahrenheit é: ", Fahrenheit)
-("--------------------------------------------------")
+
